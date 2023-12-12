efcff56b1c2499db0d75bee6af12572e4f62aa0f
Marlena Müller <git@marlena.app>
2023-12-12 10:14:31 +0100

    add minimal config

---
+++ hosts/hermes/configuration.nix
@@ -0,0 +1,44 @@
+{ pkgs, nixpkgs, ... }:
+let
+  ssh-keys = builtins.readFile ./../../macbookkey.pub;
+in
+{
+  imports = [
+    ./hardware-configuration.nix
+    # (import "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  users.users.root.openssh.authorizedKeys.keyFiles = [
+    (pkgs.writeText "root-ssh-key" ssh-keys)
+  ];
+
+  time.timeZone = "Europe/Berlin";
+
+
+  networking.hostName = "hermes";
+  networking.domain = "marlenawan.de";
+
+  # install git
+  environment.systemPackages = with pkgs; [
+    git
+    wget
+    curl
+    nano
+  ];
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 22 ];
+  };
+}
