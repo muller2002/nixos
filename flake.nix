82da75d929dd915348c0bfb3e8203db243b9bfbe
Marlena Müller <git@marlena.app>
2024-03-09 15:05:56 +0100

    Things went wrong...

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
             ({ config, pkgs, ... }: 
               let
                 overlay-unstable = final: prev: {
-                  unstable = nixpkgs-unstable.legacyPackages.x86_64-linux;
+                  unstable = unstable.legacyPackages.x86_64-linux;
                 };
               in
                 {
